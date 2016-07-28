193e6001ec09cc2a841a015439e270565c1f6489
Carrie Kengle <carriesteady@gmail.com>
2016-07-28 14:23:45 -0400

    neopixel example

---
+++ python/mqtt-neopixels.py
@@ -0,0 +1,60 @@
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+import time
+
+from neopixel import *
+import paho.mqtt.client as mqtt
+
+
+# LED strip configuration:
+LED_COUNT      = 30      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+def HTMLColorToRGB(colorstring):
+    """ convert #RRGGBB to an (R, G, B) tuple """
+    colorstring = colorstring.strip()
+    if colorstring[0] == '#': colorstring = colorstring[1:]
+    if len(colorstring) != 6:
+        raise ValueError, "input #%s is not in #RRGGBB format" % colorstring
+    r, g, b = colorstring[:2], colorstring[2:4], colorstring[4:]
+    r, g, b = [int(n, 16) for n in (r, g, b)]
+    return (r, g, b)
+
+def setStripColor(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColorRGB(i, color[0], color[1], color[2])
+        strip.show()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("topic")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    color = HTMLColorToRGB(str(msg.payload))
+    setStripColor(strip, color)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+        # Create NeoPixel object with appropriate configuration.
+        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        # Intialize the library (must be called once before other functions).
+        strip.begin()
+
+        print 'Press Ctrl-C to quit.'
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("127.0.0.1", 1883, 60)
+client.loop_forever()
